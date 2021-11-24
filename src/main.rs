2c130f948488de4c5b13b34a70276ec49ed14557
Gustav Sörnäs <gustav@sornas.net>
2021-11-24 23:55:44 +0100

    full display animation

--- src/main.rs
+++ src/main.rs
@@ -4,19 +4,41 @@
 #![no_std]
 #![no_main]
 
-use arduino_hal::prelude::*;
-use arduino_hal::usart::Usart;
-
-use tinyvec::ArrayVec;
 use ws2812_spi::prerendered::Ws2812;
 
-use smart_leds::{
-    hsv::{hsv2rgb, Hsv},
-    SmartLedsWrite, RGB,
-};
+use embedded_hal::spi::FullDuplex;
+use smart_leds::{RGB, RGB8, SmartLedsWrite, hsv::{hsv2rgb, Hsv}};
 
-use nb::block;
 use panic_halt as _;
+
+struct UnicornHAT<'b, SPI> {
+    pixels: [RGB8; 64],
+    inner: Ws2812<'b, SPI>,
+}
+
+impl<'b, SPI> UnicornHAT<'b, SPI>
+where SPI: FullDuplex<u8> {
+    fn new(ws2812: Ws2812<'b, SPI>) -> Self {
+        Self {
+            pixels: [RGB {r: 0, g: 0, b: 0}; 64],
+            inner: ws2812,
+        }
+    }
+
+    fn send(&mut self) -> Result<(), SPI::Error> {
+        self.inner.write(self.pixels.iter().cloned())
+    }
+
+    fn set_at(&mut self, x: usize, y: usize, c: RGB8) {
+        assert!(x < 8);
+        assert!(y < 8);
+        self.pixels[y * 8 + x] = c;
+    }
+
+    fn set_all(&mut self, c: RGB8) {
+        self.pixels = [c; 64];
+    }
+}
 
 #[arduino_hal::entry]
 fn main() -> ! {
@@ -24,13 +46,7 @@ fn main() -> ! {
     let pins = arduino_hal::pins!(dp);
     let mut adc = arduino_hal::Adc::new(dp.ADC, Default::default());
 
-    let a0 = pins.a0.into_analog_input(&mut adc);
-
-    // Digital pin 13 is also connected to an onboard LED marked "L"
-    // let mut led = pins.d13.into_output();
-    // led.set_high();
-
-    let mut buffer = [0; 512];
+    let mut buffer = [0; 1024];
 
     let sclk = pins.d13.into_output();
     let mosi = pins.d11.into_output();
@@ -46,38 +62,20 @@ fn main() -> ! {
         arduino_hal::spi::Settings::default(),
     );
 
-    let mut ws2812 = Ws2812::new(spi, &mut buffer);
+    let ws2812 = Ws2812::new(spi, &mut buffer);
+    let mut unicorn = UnicornHAT::new(ws2812);
 
-    arduino_hal::delay_ms(100);
-
-    ws2812.write([(0u8, 255u8, 0u8)].iter().cloned()).unwrap();
-    ws2812.write([(255u8, 0u8, 0u8)].iter().cloned()).unwrap();
-    ws2812.write([(0u8, 0u8, 255u8)].iter().cloned()).unwrap();
-
-    let mut hues = ArrayVec::<[u8; 9]>::new();
+    let mut i: u8 = 0;
 
     loop {
-        let hue = (a0.analog_read(&mut adc) / 4) as u8;
-
-        hues.insert(0, hue);
-        hues.truncate(8);
-
-        ws2812
-            .write(
-                hues.iter()
-                    .cloned()
-                    .map(|hue| {
-                        hsv2rgb(Hsv {
-                            hue,
-                            sat: 255,
-                            val: 16,
-                        })
-                    })
-            )
-            .unwrap();
-        // let val = adc.read_blocking(&a0);
-        arduino_hal::delay_ms(50);
-
-        // ufmt::uwriteln!(&mut serial, "{}!\r", val).void_unwrap();
+        for y in 0..8 {
+            for x in 0..8 {
+                let offset = y * 32 + x * 4;
+                unicorn.set_at(x, y, hsv2rgb(Hsv { hue: i.overflowing_add(offset as u8).0, sat: 255, val: 16 }));
+            }
+        }
+        unicorn.send().unwrap();
+        i = i.overflowing_add(1).0;
+        arduino_hal::delay_ms(1);
     }
 }
