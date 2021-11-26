2e464a226bcf70baffa37f02644e328cb078f27f
Gustav Sörnäs <gustav@sornas.net>
2021-11-26 10:06:44 +0100

    snake

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,20 @@
 #![no_std]
 #![no_main]
 
+use arraydeque::ArrayDeque;
 use ws2812_spi::prerendered::Ws2812;
 
 use embedded_hal::spi::FullDuplex;
 use smart_leds::{SmartLedsWrite, RGB, RGB8};
 
 use panic_halt as _;
+
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
 
 struct UnicornHAT<'b, SPI> {
     pixels: [RGB8; 64],
@@ -34,6 +42,7 @@ where
     fn set_at(&mut self, x: usize, y: usize, c: RGB8) {
         assert!(x < 8);
         assert!(y < 8);
+        //TODO (x=0, y=0) doesn't match the usual (0, 0). should be either top left or bottom left
         if y % 2 == 0 {
             self.pixels[y * 8 + x] = c;
         } else {
@@ -43,6 +52,12 @@ where
 
     fn set_all(&mut self, c: RGB8) {
         self.pixels = [c; 64];
+    }
+
+    fn set_snake<'i, I: IntoIterator<Item = &'i (usize, usize)>>(&mut self, snake: I) {
+        for (x, y) in snake.into_iter() {
+            self.set_at(*x, *y, RGB8 { r: 32, g: 0, b: 0 });
+        }
     }
 }
 
@@ -77,40 +92,60 @@ fn main() -> ! {
     let ws2812 = Ws2812::new(spi, &mut buffer);
     let mut unicorn = UnicornHAT::new(ws2812);
 
-    let mut x: u8 = 4;
-    let mut y: u8 = 4;
-    let mut moving = false;
+    let mut snake: ArrayDeque<[(usize, usize); 64]> = ArrayDeque::new();
+    snake.push_front((4, 4)).unwrap();
+    snake.push_front((5, 4)).unwrap();
+    snake.push_front((6, 4)).unwrap();
+
+    let mut dir = Direction::Right; // +x
+    let mut apple = (2, 2);
+
+    //TODO countdown? pixels in some corner
+    arduino_hal::delay_ms(2000);
+    //TODO signal initial direction with flashing
 
     loop {
         let dx = a1.analog_read(&mut adc);
         let dy = a2.analog_read(&mut adc);
 
-        if dx > 1023 - 100 && !moving {
-            x = x.wrapping_add(1);
-            moving = true;
-        } else if dx < 100 && !moving {
-            x = x.wrapping_sub(1);
-            moving = true;
-        } else if dy > 1023 - 100 && !moving {
-            y = y.wrapping_add(1);
-            moving = true;
-        } else if dy < 100 && !moving {
-            y = y.wrapping_sub(1);
-            moving = true;
+        if dx > 1023 - 100 {
+            dir = Direction::Right;
+        } else if dx < 100 {
+            dir = Direction::Left;
+        } else if dy > 1023 - 100 {
+            dir = Direction::Up;
+        } else if dy < 100 {
+            dir = Direction::Down;
         }
 
-        ufmt::uwriteln!(serial, "x: {}, y: {}, dx: {}, dy: {}\r", x, y, dx, dy).unwrap();
+        let old = snake.back().unwrap();
+        let new = match dir {
+            Direction::Up => (old.0, old.1 + 1),
+            Direction::Down => (old.0, old.1 - 1),
+            Direction::Left => (old.0 - 1, old.1),
+            Direction::Right => (old.0 + 1, old.1),
+        };
 
-        if dx != 0 && dx != 1023 && dy != 0 && dy != 1023 {
-            moving = false;
+        if new.0 > 7 || new.1 > 7 {
+            loop {} //TODO you died
         }
 
-        if x < 8 && y < 8 {
-            unicorn.set_all(RGB::default());
-            unicorn.set_at(x as usize, y as usize, RGB { r: 32, g: 0, b: 0 });
-            unicorn.send().unwrap();
+        for part in snake.iter() {
+            if &new == part {
+                loop {} //TODO you died. flash collision
+            }
         }
 
-        arduino_hal::delay_ms(100);
+        if new != apple {
+            snake.pop_front();
+        }
+        snake.push_back(new).unwrap();
+
+        unicorn.set_all(RGB::default());
+        unicorn.set_at(apple.0, apple.1, RGB { r: 0, g: 32, b: 0 });
+        unicorn.set_snake(&snake); //TODO gradient from head to tail
+        unicorn.send().unwrap();
+
+        arduino_hal::delay_ms(500);
     }
 }
